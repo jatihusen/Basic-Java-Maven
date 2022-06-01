490d7e55ec55b17380b37d7b3be912984b029000
jatihusen <46706344+jatihusen@users.noreply.github.com>
2022-06-02 00:08:40 +0900

    Update Counter.java

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -16,16 +16,32 @@ public class Counter {
 		count++;
 	}
 	
-	public void increase() {
-		count++;
+	public void increaseBy(int i) {
+		count+=i;
 	}
 	
 	public void decrement() {
 		count--;
 	}
 	
-	public void decrease() {
-		count--;
+	public void decreaseBy(int i) {
+		count-=i;
+	}
+	
+	public void multiplyBy(int i){
+		count = count * i;
+	}
+	
+	public void double(){
+		multiplyBy(2);
+	}
+	
+	public void triple(){
+		multiplyBy(3)
+	}
+	
+	public boolean isCountEven(){
+		return count%2 == 0;
 	}
 	
 	public int getCount() {
